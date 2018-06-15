587da60187b05d9728e633c465ac60ed38cb373b
George Dagher <bongostar41@gmail.com>
2018-06-15 12:14:00 -0400

    george notes

--- app.rb
+++ app.rb
@@ -3,9 +3,10 @@ require 'rerun'
 require './models'
 
 
-# to generate a random string in the irb:
-#  require 'securerandom'
-#  SecureRandom.hex
+# overall the code in app.rb looks good. The biggest thing I found was
+# you did not use a layout.rb and repeated a lot of things on each page.
+
+
 set :session_secret, ENV['SEI_SESSION_SECRET']
 enable :sessions
 
@@ -13,50 +14,51 @@ enable :sessions
 
 get('/') do
     if session[:user_id]!=nil
-         user = User.find(session[:user_id]) 
-        @user=user
+        # we don't need two variables here
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
 	erb :index
 end
 
 get('/signup') do
-     unless session[:user_id]==nil
-         user = User.find(session[:user_id]) 
-        @user=user
+    unless session[:user_id]==nil
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
 	erb :signup
 end
 
 get('/login') do
      unless session[:user_id]==nil
-         user = User.find(session[:user_id]) 
-        @user=user
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
 	erb :login
 end
 
 get('/logout') do 
-    
     session[:user_id] = nil
 	erb :index	
 end
 
 get '/cancelaccount' do 
-   user = User.find(session[:user_id])
-    p session[:user_id]
-    @user=user
+    @user = User.find(session[:user_id])
+    # @user=user
     
-    user.destroy
-    user.posts.destroy_all
-     session[:user_id]=nil
+    # should destroy the posts before destroying the user.
+    @user.posts.destroy_all
+    @user.destroy
+    
+    session[:user_id]=nil
     erb :index
 end
 
 
 get('/about') do
     unless session[:user_id]==nil
-         user = User.find(session[:user_id]) 
-        @user=user
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
 	erb :about
 end
@@ -64,26 +66,26 @@ end
 get('/blogdetails') do
     @posts = Post.all
     @users = User.all
-   if session[:user_id]!=nil
-         user = User.find(session[:user_id]) 
-        @user=user
+    if session[:user_id]!=nil
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
     
 	erb :blogdetails
 end
 
 get '/myposts' do
-   unless session[:user_id]==nil
-         user = User.find(session[:user_id]) 
-        @user=user
+    unless session[:user_id]==nil
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
     erb :myposts
 end
 
 get '/userposts' do 
-     if session[:user_id]!=nil
-         user = User.find(session[:user_id]) 
-        @user=user
+    if session[:user_id]!=nil
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
    
     @selecteduser = selecteduser
@@ -91,34 +93,32 @@ get '/userposts' do
 end
 
 get '/userposts/:id' do
-   @posts = Post.all
+    @posts = Post.all
     @users = User.all
     if session[:user_id]!=nil
-         user = User.find(session[:user_id]) 
-        @user=user
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
     selecteduser = User.find(params[:id])
     @selecteduser = selecteduser
     
    redirect  '/userposts'
-#    erb :userposts
 end
 
 
 get('/contact') do
     if session[:user_id]!=nil
-         user = User.find(session[:user_id]) 
-        @user=user
+        @user = User.find(session[:user_id]) 
+        # @user=user
     end
 	erb :contact
 end
 
 get '/deletepost/:id' do
       
-   post = Post.find(params[:id])
+    post = Post.find(params[:id])
     post.destroy
     redirect '/blogdetails'
-
 end
 
 post('/signup') do
@@ -137,35 +137,25 @@ post('/signup') do
 	redirect '/'
 end
 
-
-
 post('/login') do
-	
-
 	user = User.find_by(email: params[:email])
 	if user.nil?
-        p " my_session " , session[:user_id] 
 		return redirect '/login'
 	end
 
 	unless user.password == params[:password]
-        p " my_session " , session[:user_id] 
 		return redirect '/login'
 	end
-   p " my_session " , session[:user_id] 
 	session[:user_id] = user.id
-    p " my_session " , session[:user_id] 
 	redirect '/'
 end
 
 post '/blogpost' do
-   user_id = session[:user_id]
+    user_id = session[:user_id]
 	if user_id.nil?
 		return redirect '/login'
 	end
    
-
-    
     @userpost = Post.create(
         user_id: user_id,
 		subject: params[:subject],
@@ -173,5 +163,4 @@ post '/blogpost' do
   	)
 	
 	redirect '/blogdetails'
-    
 end
